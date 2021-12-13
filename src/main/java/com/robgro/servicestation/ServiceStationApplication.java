dde1812f2109429a5e1d79ea570109ddf50f8e8d
RobGro <r.grodzki74@gmail.com>
2021-12-13 20:59:53 +0000

    Initial commit for Service Station application

---
+++ src/main/java/com/robgro/servicestation/ServiceStationApplication.java
@@ -0,0 +1,13 @@
+package com.robgro.servicestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceStationApplication.class, args);
+    }
+
+}

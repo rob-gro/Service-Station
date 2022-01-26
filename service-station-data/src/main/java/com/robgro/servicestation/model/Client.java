d90332faeb31283632bea9a60b34cb365dbd0428
RobGro <r.grodzki74@gmail.com>
2022-01-26 13:32:12 +0000

    Added personal's data to Client and Mechanic

--- service-station-data/src/main/java/com/robgro/servicestation/model/Client.java
+++ service-station-data/src/main/java/com/robgro/servicestation/model/Client.java
@@ -6,6 +6,46 @@ public class Client extends Person {
 
     private Set<Car> cars;
 
+    @Override
+    public String getFirstName() {
+        return super.getFirstName();
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        super.setFirstName(firstName);
+    }
+
+    @Override
+    public String getLastName() {
+        return super.getLastName();
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        super.setLastName(lastName);
+    }
+
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email);
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return super.getPhoneNumber();
+    }
+
+    @Override
+    public void setPhoneNumber(String phoneNumber) {
+        super.setPhoneNumber(phoneNumber);
+    }
+
     public Set<Car> getCars() {
         return cars;
     }
--- service-station-data/src/main/java/com/robgro/servicestation/model/Mechanic.java
+++ service-station-data/src/main/java/com/robgro/servicestation/model/Mechanic.java
@@ -13,4 +13,44 @@ public class Mechanic extends Person{
     public void setSpecializations(Set<Specialization> specializations) {
         this.specializations = specializations;
     }
+
+    @Override
+    public String getFirstName() {
+        return super.getFirstName();
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        super.setFirstName(firstName);
+    }
+
+    @Override
+    public String getLastName() {
+        return super.getLastName();
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        super.setLastName(lastName);
+    }
+
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email);
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return super.getPhoneNumber();
+    }
+
+    @Override
+    public void setPhoneNumber(String phoneNumber) {
+        super.setPhoneNumber(phoneNumber);
+    }
 }

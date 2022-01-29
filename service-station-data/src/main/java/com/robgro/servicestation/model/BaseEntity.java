977b93e0312aa20d9f03254c86df4bdc8d43e1d5
RobGro <r.grodzki74@gmail.com>
2022-01-29 21:07:22 +0000

    Base Entity (JPA) implemented

--- service-station-data/src/main/java/com/robgro/servicestation/model/BaseEntity.java
+++ service-station-data/src/main/java/com/robgro/servicestation/model/BaseEntity.java
@@ -2,11 +2,16 @@ package com.robgro.servicestation.model;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {

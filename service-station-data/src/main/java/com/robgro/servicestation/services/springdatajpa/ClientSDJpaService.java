6439f5e5e8f0a23053e87b4126d2a378867bd5f2
RobGro <r.grodzki74@gmail.com>
2022-01-31 21:02:20 +0000

    added client JPA service

---
+++ service-station-data/src/main/java/com/robgro/servicestation/services/springdatajpa/ClientSDJpaService.java
@@ -0,0 +1,60 @@
+package com.robgro.servicestation.services.springdatajpa;
+
+import com.robgro.servicestation.model.Client;
+import com.robgro.servicestation.repositories.CarModelRepository;
+import com.robgro.servicestation.repositories.CarRepository;
+import com.robgro.servicestation.repositories.ClientRepository;
+import com.robgro.servicestation.services.ClientService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class ClientSDJpaService implements ClientService {
+
+    private final ClientRepository clientRepository;
+    private final CarRepository carRepository;
+    private final CarModelRepository carModelRepository;
+
+    public ClientSDJpaService(ClientRepository clientRepository, CarRepository carRepository,
+                              CarModelRepository carModelRepository) {
+        this.clientRepository = clientRepository;
+        this.carRepository = carRepository;
+        this.carModelRepository = carModelRepository;
+    }
+
+    @Override
+    public Client findByLastName(String lastName) {
+        return clientRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Client> findAll() {
+        Set<Client> clients = new HashSet<>();
+        clientRepository.findAll().forEach(clients::add);
+        return clients;
+    }
+
+    @Override
+    public Client findById(Long aLong) {
+        return clientRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Client save(Client object) {
+        return clientRepository.save(object);
+    }
+
+    @Override
+    public void delete(Client object) {
+        clientRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        clientRepository.deleteById(aLong);
+    }
+}

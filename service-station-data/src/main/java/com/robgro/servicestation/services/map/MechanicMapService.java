a9313138041af344ea60c2dc49d20daf37b116bf
RobGro <r.grodzki74@gmail.com>
2022-01-31 21:38:21 +0000

    added mechanic save functionality

--- service-station-data/src/main/java/com/robgro/servicestation/services/map/MechanicMapService.java
+++ service-station-data/src/main/java/com/robgro/servicestation/services/map/MechanicMapService.java
@@ -1,13 +1,21 @@
 package com.robgro.servicestation.services.map;
 
 import com.robgro.servicestation.model.Mechanic;
+import com.robgro.servicestation.model.Specialization;
 import com.robgro.servicestation.services.MechanicService;
+import com.robgro.servicestation.services.SpecializationService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class MechanicMapService extends AbstractMapService<Mechanic, Long> implements MechanicService {
+
+    private final SpecializationService specializationService;
+
+    public MechanicMapService(SpecializationService specializationService) {
+        this.specializationService = specializationService;
+    }
 
     @Override
     public Set<Mechanic> findAll() {
@@ -21,6 +29,14 @@ public class MechanicMapService extends AbstractMapService<Mechanic, Long> imple
 
     @Override
     public Mechanic save(Mechanic object) {
+        if (object.getSpecializations().size() > 0) {
+            object.getSpecializations().forEach(specialization -> {
+                if (specialization.getId() == null) {
+                    Specialization savedSpecialization = specializationService.save(specialization);
+                    specialization.setId(savedSpecialization.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
